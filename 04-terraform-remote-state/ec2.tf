4aea349092c6faf0289166b42bc0a2531a825bd9
Manohar <manohar015@gmail.com>
2022-12-11 19:44:33 +0530

    Added my Comments

--- 04-terraform-remote-state/ec2.tf
+++ 04-terraform-remote-state/ec2.tf
@@ -5,15 +5,15 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami = "ami-06876ae57f920d87d"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allows_ssh.id]
   
   tags = {
     "Name" = "valLinux Terraform"
   }
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+resource "aws_security_group" "allows_ssh" {
+  name        = "allows_ssh"
   description = "Allow ssh inbound traffic"
 
 
@@ -35,6 +35,6 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   tags = {
-    Name = "allow_ssh"
+    Name = "allows_ssh"
   }
 }
