ec0ff4f53366bdd942e20090e6fbf3553af85961
Manohar <manohar015@gmail.com>
2022-12-11 19:18:43 +0530

    Added my Comments

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -5,8 +5,36 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami = "ami-06876ae57f920d87d"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = aws_security_group.allow_ssh.id
+  
   tags = {
     "Name" = "valLinux Terraform"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
