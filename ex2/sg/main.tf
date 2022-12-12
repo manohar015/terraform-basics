dbc22e4badb8f74bb5c29cf9c2e91625d3b580e4
Manohar <manohar015@gmail.com>
2022-12-12 09:06:14 +0530

    Added my Comments

--- ex2/sg/main.tf
+++ ex2/sg/main.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "allowed_ssh" {
     
   }
 
+  ingress {
+    description      = "HTTP from internet"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
   egress {
     from_port        = 0
     to_port          = 0
