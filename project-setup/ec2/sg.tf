de3f864fc34c1324c1707a811630dd805793560a
Manohar <manohar015@gmail.com>
2022-12-12 11:15:24 +0530

    Added my Comments

--- project-setup/ec2/sg.tf
+++ project-setup/ec2/sg.tf
@@ -5,30 +5,14 @@ resource "aws_security_group" "allowed_ssh" {
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     
   }
 
-  ingress {
-    description      = "HTTP from internet"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
     
-  }
-  
-  ingress {
-    description      = "HTTP from internet"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
-  }
   egress {
     from_port        = 0
     to_port          = 0
