5aa68fe3a099075603223a7fac9eca748e742626
Manohar <manohar015@gmail.com>
2022-12-12 17:42:26 +0530

    Added my Comments

--- project-setup/ec2/ec2.tf
+++ project-setup/ec2/ec2.tf
@@ -4,8 +4,8 @@ resource "aws_spot_instance_request" "spot_worker" {
   wait_for_fulfillment = true
   vpc_security_group_ids = [aws_security_group.allowed_ssh.id]
 
-tags = {
-    "Name" = "Mongodb"
+ tags = {
+    "Name" = "Linux Terraform"
   }
 
   provisioner "remote-exec" {
