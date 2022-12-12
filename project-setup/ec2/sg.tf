dc28b981bdb8523025c42bd613787ce1be8b9733
Manohar <manohar015@gmail.com>
2022-12-12 11:10:16 +0530

    Added my Comments

--- project-setup/ec2/sg.tf
+++ project-setup/ec2/sg.tf
@@ -20,6 +20,15 @@ resource "aws_security_group" "allowed_ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
     
   }
+  
+  ingress {
+    description      = "HTTP from internet"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
   egress {
     from_port        = 0
     to_port          = 0
