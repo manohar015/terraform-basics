63a141ef25406510a96f8454fb95038390627cef
Manohar <manohar015@gmail.com>
2022-12-12 10:20:53 +0530

    Added my Comments

--- project-setup/provider.tf
+++ project-setup/provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
     region = "us-east-1"
   
 }
+
+terraform {
+  backend "s3" {
+    bucket = "manohar-b50-tf-state-bucket"
+    key    = "project/env/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
