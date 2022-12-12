f2e255e50b5005afd864ca868b135003068e4412
Manohar <manohar015@gmail.com>
2022-12-12 18:11:17 +0530

    Added my Comments

--- project-setup/ec2/ec2.tf
+++ project-setup/ec2/ec2.tf
@@ -3,9 +3,9 @@ resource "aws_spot_instance_request" "spot_worker" {
   instance_type = "t2.micro"
   wait_for_fulfillment = true
   vpc_security_group_ids = [aws_security_group.allowed_ssh.id]
-
+    
 tags = {
-    Name = "CheapWorker"
+    aws_instance_tag  = "CheapWorker"
   }
 
   provisioner "remote-exec" {
