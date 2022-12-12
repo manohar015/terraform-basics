8f5537ac3899705cea3ebeab493b0aa0eff26d5d
Manohar <manohar015@gmail.com>
2022-12-12 17:40:09 +0530

    Added my Comments

--- project-setup/ec2/ec2.tf
+++ project-setup/ec2/ec2.tf
@@ -5,7 +5,7 @@ resource "aws_spot_instance_request" "spot_worker" {
   vpc_security_group_ids = [aws_security_group.allowed_ssh.id]
 
 tags = {
-    "Name" = var.COMPONENT
+    "Name" = "${var.COMPONENT}"
   }
 
   provisioner "remote-exec" {
