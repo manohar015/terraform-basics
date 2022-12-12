a3a626168b0c6bd35611561e284a5c9882cc8668
Manohar <manohar015@gmail.com>
2022-12-12 17:30:58 +0530

    Added my Comments

--- project-setup/vars.tf
+++ project-setup/vars.tf
@@ -2,10 +2,8 @@ variable "ALL_COMPONENTS" {
     default = {
         mongodb = {
             app_version ="null"
-        },
-        mysql = {
-            app_version ="null"
         }
+       
 
     }
 }
