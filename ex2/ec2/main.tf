ec4cf5524bcb659baf59392a36fa5b62508a3ace
Manohar <manohar015@gmail.com>
2022-12-12 08:51:40 +0530

    Added my Comments

--- ex2/ec2/main.tf
+++ ex2/ec2/main.tf
@@ -3,15 +3,6 @@ resource "aws_instance" "demo1" {
   instance_type = "t2.micro"
   vpc_security_group_ids = [var.sg]
   
-  provisioner "local-exec" {
-    comand = <<EOF
-    sleep 20,
-    cd /home/centos/ansible,
-    ansible-playbook -i ${self.public_ip}  roboshop.yml -e COMPONENT=mongodb -e ENV=dev -e ansible_user=centos -e ansible_password=DevOps321
-  EOF
-  }
-
-
   tags = {
     "Name" = "Linux Terraform"
   }
