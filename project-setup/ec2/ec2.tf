2926072a9561e7ace8ce451a0b9c2fd2e70c037a
Manohar <manohar015@gmail.com>
2022-12-12 10:24:23 +0530

    Added my Comments

--- project-setup/ec2/ec2.tf
+++ project-setup/ec2/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_spot_instance_request" "spot_worker" {
   ami = data.aws_ami.my_ami.id
   instance_type = "t2.micro"
   wait_for_fulfillment = true
+  vpc_security_group_ids = [aws_security_group.allowed_ssh.id]
 
   tags = {
     "Name" = "var.COMPONENT"
---
+++ project-setup/ec2/sg.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "allowed_ssh" {
+  name        = "allowed_${var.COMPONENT}"
+  description = "Allow ssh inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  ingress {
+    description      = "HTTP from internet"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "allowed_${var.COMPONENT}"
+  }
+}
+
+output "sg_id" {
+value = aws_security_group.allowed_ssh.id
+ }
